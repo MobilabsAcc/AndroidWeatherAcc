463b71e69643e8845c432a921f989a55b714ac71
lukasz.gronowski <lukasz.gronowski@accenture.com>
2020-03-15 11:18:29 +0100

    added latest dependencies to libraries which we will use. Added navigation graph and navigation from splash to first fragment

--- app/src/main/java/eu/vmpay/weatheracc/MainActivity.kt
+++ app/src/main/java/eu/vmpay/weatheracc/MainActivity.kt
@@ -1,18 +1,12 @@
 package eu.vmpay.weatheracc
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import eu.vmpay.weatheracc.ui.fragment.SplashFragment
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, SplashFragment.newInstance())
-                    .commitNow()
-        }
     }
 }
--- app/src/main/java/eu/vmpay/weatheracc/ui/fragment/ForecastListFragment.kt
+++ app/src/main/java/eu/vmpay/weatheracc/ui/fragment/ForecastListFragment.kt
@@ -1,24 +1,28 @@
 package eu.vmpay.weatheracc.ui.fragment
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
 import eu.vmpay.weatheracc.R
 import eu.vmpay.weatheracc.viewModels.ForecastListViewModel
 
 class ForecastListFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ForecastListFragment()
-    }
-
     private lateinit var viewModel: ForecastListViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+        requireActivity().onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    activity?.finish()
+                }
+            })
         return inflater.inflate(R.layout.forecast_list_fragment, container, false)
     }
 
